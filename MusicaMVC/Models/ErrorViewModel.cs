09f9763273779e9145445e2199c8bef03c4e41e6
Emmanouil Gimissis <manolis@pgimisis.gr>
2021-09-20 10:02:00 +0100

    Add MVC project

---
+++ MusicaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
